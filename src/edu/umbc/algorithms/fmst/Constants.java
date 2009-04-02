a22cc7f49c6ea394e70c8b0e8f11af4f304f89b6
fatihsenel <fatihsenel@02d13462-1c76-11de-b56b-c9554f25afe1>
2009-04-02 16:50:38 +0000

    Constants will be here

---
+++ src/edu/umbc/algorithms/fmst/Constants.java
@@ -0,0 +1,11 @@
+package edu.umbc.algorithms.fmst;
+
+/**
+ * @author : Fatih Senel
+ *         Date: Apr 2, 2009
+ *         Time: 12:48:37 PM
+ */
+public interface Constants {
+    double MAX_STDEV = 20;//to be need to changes
+    double MAX_PCR_ALLOWED = 100;
+}
