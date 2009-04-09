b0d088db8659eec8940fa33249e485434d0b862a
nielskasch <nielskasch@hotmail.com>
2009-04-09 02:44:23 +0000

    Changed how real steiner nodes are moved.

--- src/edu/umbc/algorithms/fmst/Constants.java
+++ src/edu/umbc/algorithms/fmst/Constants.java
@@ -6,9 +6,9 @@ package edu.umbc.algorithms.fmst;
  *         Time: 12:48:37 PM
  */
 public interface Constants {
-    Double MAX_STDEV = 500.0;  //to be need to changes
+    Double TARGET_STDEV = 100.0;  //to be need to changes
     Double MAX_PCR_ALLOWED = 4000.0;
-    int MAX_NEW_STEINER_NODES = 10;
+    int MAX_RELAY_NODES = 50;
     Double TRANSMISSION_RANGE = Math.sqrt(MAX_PCR_ALLOWED);
     int CONVERGENCE_CUTOFF = 5;  //when we see the same STDEV  CONVERGENCE_CUTOFF times in a row, the we converged
     int CONVERGENCE_THRESHOLD = 10;  //the difference of the standard deviation between loops must be greater
